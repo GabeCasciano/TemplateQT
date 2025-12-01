c64ccfeec9f7ce938ab17a6bd7b14086ea29d213
GabeCasciano <gabecasciano@gmail.com>
2025-12-01 13:55:58 -0500

    initial commit and .gitignore

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from PyQt5.QtWidgets import QApplication
+import sys, signal
+import os
+from include.MainWindow import MainWindow
+
+app = QApplication(sys.argv)
+app.setStyle("Fusion") # for windows
+
+
+def handle_signals(*args):
+    app.quit()
+
+if __name__ == "__main__":
+    # Check os and set xcb
+    if os.name != "nt":
+        os.environ["QT_QPA_PLATFORM"] = "xcb"
+
+    # handle signals
+    signal.signal(signal.SIGINT, handle_signals)
+    signal.signal(signal.SIGTERM, handle_signals)
+
+    win = MainWindow()
+    win.show()
+
+    sys.exit(app.exec_())
